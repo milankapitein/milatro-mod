2aeab9086fda79e930a07709d72f956b3e0e7ed8
Milan Kapitein <milankapitein@gmail.com>
2025-04-23 18:52:51 +0200

    Fixed including decks

--- milatro.lua
+++ milatro.lua
@@ -1,6 +1,2 @@
 SMODS.load_file("src/jokers.lua")()
-<<<<<<< HEAD
--- SMODS.load_file("src/decks.lua")()
-=======
 SMODS.load_file("src/decks.lua")()
->>>>>>> 479ca0bcdce08c4c817fa4ef9d768f410866ee05
