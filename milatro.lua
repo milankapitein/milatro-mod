cc59a437ecf4121ff08bbc077b15ca219354df63
Milan Kapitein <milankapitein@gmail.com>
2025-04-26 20:41:15 +0200

    Made my first boss blind work

--- milatro.lua
+++ milatro.lua
@@ -1,6 +1,8 @@
 SMODS.load_file("src/jokers.lua")()
 SMODS.load_file("src/decks.lua")()
 SMODS.load_file("src/vouchers.lua")()
+SMODS.load_file("src/blinds.lua")()
+
 
 SMODS.Atlas {
 	-- Key for code to find it with
@@ -22,4 +24,19 @@ SMODS.Atlas {
 	px = 71,
 	-- Height of each sprite in 1x size
 	py = 95
+}
+
+SMODS.Atlas {
+	-- Key for code to find it with
+	key = "MilatroBlinds",
+
+	atlas_table = "ANIMATION_ATLAS",
+
+	frames = 21,
+	-- The name of the file, for the code to pull the atlas from
+	path = "tempBlind.png",
+	-- Width of each sprite in 1x size
+	px = 35,
+	-- Height of each sprite in 1x size
+	py = 35
 }
---
+++ src/blinds.lua
@@ -0,0 +1,57 @@
+-- The Count
+SMODS.Blind{
+    key = 'the_count',
+    loc_txt = {
+        name = "The Count",
+        text = {
+            "The most frequent card",
+            "in your deck is debuffed"
+        }
+    },
+
+    discovered = true,
+    unlocked = true,
+
+    atlas = "MilatroBlinds",
+    pos = {x = 0, y = 0},
+    dollars = 5,
+    mult = 2,
+
+    boss = {min = 2, max = 10},
+    boss_colour = HEX("389396"),
+
+
+    recalc_debuff = function(self, card, from_blind)
+        local freq_ranks = {
+			[2] = 0,
+			[3] = 0,
+			[4] = 0,
+			[5] = 0,
+			[6] = 0,
+			[7] = 0,
+			[8] = 0,
+			[9] = 0,
+			[10] = 0,
+			[11] = 0,
+			[12] = 0,
+			[13] = 0,
+			[14] = 0,
+		}
+		for k, v in pairs(G.playing_cards) do
+			if v:get_id() > 0 then
+				freq_ranks[v:get_id()] = freq_ranks[v:get_id()] + 1
+			end
+		end
+		local highestFreq = 14
+		for i = 14, 2, -1 do
+			if freq_ranks[i] > freq_ranks[highestFreq] then
+				highestFreq = i
+			end
+		end
+		if card:get_id() == highestFreq then
+			card:set_debuff(true)
+			return true
+		end
+        return false
+    end
+}
